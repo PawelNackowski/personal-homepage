6abb2782fadde7ca24a5d705b646566e2660c177
Pawel Nackowski <pawelnackowski@gmail.com>
2023-03-03 17:59:51 +0100

    create theme

---
+++ src/theme.js
@@ -0,0 +1,10 @@
+export const theme = {
+  color: {
+    white: "#ffffff",
+    mercury: "#E5E5E5",
+    mischla: "#D1D5DB",
+    scinceBlue: "#0366D6",
+    slateGray: "#6E7E91",
+    tundora: "#252525",
+  }
+}
