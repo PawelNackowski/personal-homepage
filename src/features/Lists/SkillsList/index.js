74c6d78258e975d75e44abbadc64940cad45d1ff
Pawel Nackowski <pawelnackowski@gmail.com>
2023-03-05 08:04:47 +0100

    create skillsList

---
+++ src/features/Lists/SkillsList/index.js
@@ -0,0 +1,23 @@
+import { ListItem } from "../styled"
+
+export const SkillsList = () => {
+  return (
+    <>
+      <ListItem>test 1</ListItem>
+      <ListItem>test 1</ListItem>
+      <ListItem>test 1</ListItem>
+      <ListItem>test 1</ListItem>
+      <ListItem>test 1</ListItem>
+      <ListItem>test 1</ListItem>
+      <ListItem>test 1</ListItem>
+      <ListItem>test 1</ListItem>
+      <ListItem>test 1</ListItem>
+      <ListItem>test 1</ListItem>
+      <ListItem>test 1</ListItem>
+      <ListItem>test 1</ListItem>
+      <ListItem>test 1</ListItem>
+      <ListItem>test 1</ListItem>
+      <ListItem>test 1</ListItem>
+    </>
+  )
+}
