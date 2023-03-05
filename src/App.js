286c50e841aeea40767e6c47e1572c90d50e554e
Pawel Nackowski <pawelnackowski@gmail.com>
2023-03-05 16:06:20 +0100

    added projects section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Container } from "./common/Container";
 import { FeatureList } from "./features/Lists/FeatureList";
 import { SkillsList } from "./features/Lists/SkillsList";
+import { ProjectSection } from "./features/Projects";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         title={"What I want to learn next 🚀"}
         body={<FeatureList />}
       />
+      <ProjectSection />
     </>
   );
 }
