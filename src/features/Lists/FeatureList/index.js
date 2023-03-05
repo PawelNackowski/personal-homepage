837d6831fc8435ef13e2b602214367f122717ec3
Pawel Nackowski <pawelnackowski@gmail.com>
2023-03-05 08:05:01 +0100

    created featureList

---
+++ src/features/Lists/FeatureList/index.js
@@ -0,0 +1,14 @@
+import { ListItem } from "../styled";
+
+export const FeatureList = () => {
+  return (
+    <>
+      <ListItem>test 2</ListItem>
+      <ListItem>test 2</ListItem>
+      <ListItem>test 2</ListItem>
+      <ListItem>test 2</ListItem>
+      <ListItem>test 2</ListItem>
+      <ListItem>test 2</ListItem>
+    </>
+  )
+}
