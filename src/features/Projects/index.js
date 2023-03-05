ca5a404aae2973d1de2c5e6e1ff857566a13739c
Pawel Nackowski <pawelnackowski@gmail.com>
2023-03-05 16:06:03 +0100

    added projects component

---
+++ src/features/Projects/index.js
@@ -0,0 +1,35 @@
+import { Tile } from "../../common/Tile"
+import { Wrapper } from "./styled"
+
+export const ProjectSection = () => {
+  return (
+    <>
+      <Wrapper>
+        <Tile
+          title={"Nagłówek"}
+          content={"content"}
+          linkDemo={"link"}
+          linkRepo={"link"}
+        />
+        <Tile
+          title={"Nagłówek"}
+          content={"content"}
+          linkDemo={"link"}
+          linkRepo={"link"}
+        />
+        <Tile
+          title={"Nagłówek"}
+          content={"content"}
+          linkDemo={"link"}
+          linkRepo={"link"}
+        />
+        <Tile
+          title={"Nagłówek"}
+          content={"content"}
+          linkDemo={"link"}
+          linkRepo={"link"}
+        />
+      </Wrapper>
+    </>
+  )
+}
