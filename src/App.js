21da36419e50fdbfd7536fcc5ac94c67267854d6
Pawel Nackowski <pawelnackowski@gmail.com>
2023-03-05 08:01:10 +0100

    added sections skillsList and featureList in app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import { Container } from "./common/Container";
+import { FeatureList } from "./features/Lists/FeatureList";
+import { SkillsList } from "./features/Lists/SkillsList";
 
 function App() {
   return (
-   <div>Test</div>
+    <>
+      <Container
+        title={"My skillset includes 🛠️"}
+        body={<SkillsList />} />
+      <Container
+        title={"What I want to learn next 🚀"}
+        body={<FeatureList />}
+      />
+    </>
   );
 }
 
