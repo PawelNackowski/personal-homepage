bdfa9afa67255f5e7c5439ffc9d0c31b28ca75e0
Pawel Nackowski <pawelnackowski@gmail.com>
2023-03-07 13:34:15 +0100

    changed name colors

--- src/theme.js
+++ src/theme.js
@@ -2,9 +2,9 @@ export const theme = {
   
   color: {
     white: "#ffffff",
-    ultraLightBlue:"#0366d633",
     lightBlue: "#6E7E91",
     blue: "#0366D6",
+    darkBlue:"#0366d633",
     lightGray: "#fbfbfe",
     darkerGray:"#d1d5da4d",
     black: "#252525",
