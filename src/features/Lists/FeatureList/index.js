a96f1614cd90b568bd4af6789750d03a22d4f8b1
Pawel Nackowski <pawelnackowski@gmail.com>
2023-03-09 07:49:42 +0100

    edited list

--- src/features/Lists/FeatureList/index.js
+++ src/features/Lists/FeatureList/index.js
@@ -3,12 +3,13 @@ import { ListItem } from "../styled";
 export const FeatureList = () => {
   return (
     <>
-      <ListItem>test 2</ListItem>
-      <ListItem>test 2</ListItem>
-      <ListItem>test 2</ListItem>
-      <ListItem>test 2</ListItem>
-      <ListItem>test 2</ListItem>
-      <ListItem>test 2</ListItem>
+      <ListItem>Reaact Content</ListItem>
+      <ListItem>Type Script</ListItem>
+      <ListItem>Angular</ListItem>
+      <ListItem>Node.js</ListItem>
+      <ListItem>JS classes</ListItem>
+      <ListItem>C++</ListItem>
+      <ListItem>C#</ListItem>
     </>
   )
 }
