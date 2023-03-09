a1887b7d6f32521f91c8b6373e5e8f7892510e31
Pawel Nackowski <pawelnackowski@gmail.com>
2023-03-09 07:45:34 +0100

    edited list

--- src/features/Lists/SkillsList/index.js
+++ src/features/Lists/SkillsList/index.js
@@ -3,21 +3,28 @@ import { ListItem } from "../styled"
 export const SkillsList = () => {
   return (
     <>
-      <ListItem>test 1</ListItem>
-      <ListItem>test 1</ListItem>
-      <ListItem>test 1</ListItem>
-      <ListItem>test 1</ListItem>
-      <ListItem>test 1</ListItem>
-      <ListItem>test 1</ListItem>
-      <ListItem>test 1</ListItem>
-      <ListItem>test 1</ListItem>
-      <ListItem>test 1</ListItem>
-      <ListItem>test 1</ListItem>
-      <ListItem>test 1</ListItem>
-      <ListItem>test 1</ListItem>
-      <ListItem>test 1</ListItem>
-      <ListItem>test 1</ListItem>
-      <ListItem>test 1</ListItem>
+      <ListItem>Semantic & accessible HTML</ListItem>
+      <ListItem>React Router</ListItem>
+      <ListItem>Promises, Async/Await</ListItem>
+      <ListItem>Responsive Web Design</ListItem>
+      <ListItem>Redux-Saga</ListItem>
+      <ListItem>GitHub Pull Requests & Review</ListItem>
+      <ListItem>Teamwork</ListItem>
+      <ListItem>Redux (Toolkit)</ListItem>
+      <ListItem>Trello</ListItem>
+      <ListItem>Markdown</ListItem>
+      <ListItem>React Hook</ListItem>
+      <ListItem>Scrum</ListItem>
+      <ListItem>Immutability</ListItem>
+      <ListItem>Error handling</ListItem>
+      <ListItem>NPM</ListItem>
+      <ListItem>CSS BEM convention</ListItem>
+      <ListItem>Working with API (fetch, axios)</ListItem>
+      <ListItem>React</ListItem>
+      <ListItem>CSS Grid</ListItem>
+      <ListItem>JavaScript: ES6+</ListItem>
+      <ListItem>Git</ListItem>
+      <ListItem>CSS Flexbox</ListItem>
     </>
   )
 }
