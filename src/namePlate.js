bc9c91527272055295a51e19dfcda372e64a739c
Paweł <pawelnackowski@gmail.com>
2023-04-22 09:26:18 +0200

    added props name, descripion, skilsHeader and skills

---
+++ src/namePlate.js
@@ -0,0 +1,37 @@
+export const name = "Paweł Nackowski";
+
+export const description = (
+    <>
+    👨🏻 💻 I'm a passionate Frontend Developer, 
+    i feel great in React. Currently looking 
+    for new job offers and interesting projects.
+    </>
+  );
+
+export const skillsHeader = "My skillset includes 🛠️";
+export const skills = [
+  "Semantic & accessible HTML",
+  "React Router",
+  "Promises, Async/Await",
+  "Responsive Web Design",
+  "Redux-Saga",
+  "GitHub Pull Requests & Review",
+  "Teamwork",
+  "Redux (Toolkit)",
+  "Trello",
+  "Markdown",
+  "React Hook",
+  "Scrum",
+  "Immutability",
+  "Error handling",
+  "NPM",
+  "CSS BEM convention",
+  "Working with API (fetch, axios)",
+  "React",
+  "CSS Grid",
+  "JavaScript: ES6+",
+  "Git",
+  "CSS Flexbox",
+  ];
+
+export const email = "pawelnackowski@gmail.com";
