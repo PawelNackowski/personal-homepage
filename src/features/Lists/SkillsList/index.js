417d6706c0b9003464e6845a527c2f74f37a6584
Paweł <pawelnackowski@gmail.com>
2023-04-22 09:25:33 +0200

    added props skills to render list

--- src/features/Lists/SkillsList/index.js
+++ src/features/Lists/SkillsList/index.js
@@ -1,30 +1,14 @@
+import { skills } from "../../../namePlate"
 import { ListItem } from "../styled"
 
 export const SkillsList = () => {
   return (
     <>
-      <ListItem>Semantic & accessible HTML</ListItem>
-      <ListItem>React Router</ListItem>
-      <ListItem>Promises, Async/Await</ListItem>
-      <ListItem>Responsive Web Design</ListItem>
-      <ListItem>Redux-Saga</ListItem>
-      <ListItem>GitHub Pull Requests & Review</ListItem>
-      <ListItem>Teamwork</ListItem>
-      <ListItem>Redux (Toolkit)</ListItem>
-      <ListItem>Trello</ListItem>
-      <ListItem>Markdown</ListItem>
-      <ListItem>React Hook</ListItem>
-      <ListItem>Scrum</ListItem>
-      <ListItem>Immutability</ListItem>
-      <ListItem>Error handling</ListItem>
-      <ListItem>NPM</ListItem>
-      <ListItem>CSS BEM convention</ListItem>
-      <ListItem>Working with API (fetch, axios)</ListItem>
-      <ListItem>React</ListItem>
-      <ListItem>CSS Grid</ListItem>
-      <ListItem>JavaScript: ES6+</ListItem>
-      <ListItem>Git</ListItem>
-      <ListItem>CSS Flexbox</ListItem>
+      {skills.map((skill) => (
+        <ListItem key={skill}>
+          {skill}
+          </ListItem>
+      ))}
     </>
   )
 }
