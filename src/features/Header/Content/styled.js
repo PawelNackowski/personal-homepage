96e0d55c260caccc2564169457c2371a361f2e5c
Paweł <pawelnackowski@gmail.com>
2023-04-26 16:29:04 +0200

    changed image path

--- src/features/Header/Content/styled.js
+++ src/features/Header/Content/styled.js
@@ -1,35 +1,35 @@
 import styled from "styled-components";
-import {ReactComponent as message} from "../../../common/Container/Message.svg";
+import { ReactComponent as message } from "../Message.svg";
 
 export const Wrapper = styled.div`
   align-self: center;
-`
+`;
 
 export const Header = styled.h1`
-  color: ${({theme}) => theme.color.black};
-`
+  color: ${({ theme }) => theme.color.black};
+`;
 
 export const Text = styled.div`
   font-size: 20px;
-`
+`;
 
 export const MessageIcon = styled(message)`
   width: 20px;
   height: 18px;
-`
+`;
 export const ButtonLink = styled.a`
   display: flex;
   justify-content: space-around;
   align-items: center;
-  color: ${({theme}) => theme.color.white};
+  color: ${({ theme }) => theme.color.white};
   font-size: 20px;
   text-decoration: none;
   width: 154px;
   height: 49px;
   margin-top: 32px;
   padding: 12px;
-  background-color: ${({theme}) => theme.color.blue};
-  border: 1px ${({theme}) => theme.color.darkerGray};
+  background-color: ${({ theme }) => theme.color.blue};
+  border: 1px ${({ theme }) => theme.color.darkerGray};
   border-radius: 4px;
   transition: 0.5s;
 
@@ -40,4 +40,4 @@ export const ButtonLink = styled.a`
   &:active {
     box-shadow: inset 0 2px 0 rgba(20, 70, 32, 0.2);
   }
-`
+`;
--- src/features/Header/ThemeButton/styled.js
+++ src/features/Header/ThemeButton/styled.js
@@ -1,29 +1,29 @@
-import styled from "styled-components"
-import {ReactComponent as toggleLight} from "../../../common/Container/toggleLight.svg";
+import styled from "styled-components";
+import { ReactComponent as toggleLight } from "../toggleLight.svg";
 
 export const ThemeModeButton = styled.div`
   display: flex;
   gap: 12px;
   justify-content: end;
   align-items: center;
-`
+`;
 export const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   width: 48px;
   height: 26px;
   border-radius: 25px;
-  background-color: ${({theme}) => theme.color.gray};
+  background-color: ${({ theme }) => theme.color.gray};
   border: 1px solid rgba(209, 213, 218, 0.3);
   justify-content: center;
   cursor: pointer;
-`
+`;
 
 export const ModeButton = styled(toggleLight)`
   align-self: flex-start;
   width: 20px;
   height: 20px;
-  background-color: ${({theme}) => theme.color.veryLightBlue};
+  background-color: ${({ theme }) => theme.color.veryLightBlue};
   border-radius: 50%;
   padding: 4px;
   margin: 2px;
@@ -32,4 +32,4 @@ export const ModeButton = styled(toggleLight)`
   :active {
     transform: translateX(110%);
   }
-`
+`;
