260e26f4e43b32cd84752c02eae61430a5b21f37
Paweł <pawelnackowski@gmail.com>
2023-04-26 12:10:14 +0200

    format

--- src/features/Footer/index.js
+++ src/features/Footer/index.js
@@ -1,21 +1,20 @@
-import { Socjals } from "./Socjals"
-import { email, footerContent } from "../../namePlate"
-import { Container, Content, Header, HeaderLink } from "./styled"
-
+import {Socials} from "./Socjals"
+import {email, footerContent} from "../../namePlate"
+import {Container, Content, Header, HeaderLink} from "./styled"
 export const Footer = () => {
   return (
     <>
       <Container>
         LETS TALK!
         <Header>
-          <HeaderLink href={`mailto:${email}`} >
+          <HeaderLink href={`mailto:${email}`}>
             {email}
           </HeaderLink>
         </Header>
         <Content>
           {footerContent}
         </Content>
-        <Socjals />
+        <Socials/>
       </Container>
     </>
   )
