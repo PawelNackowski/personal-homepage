e4deba37c03492e563913907eae8bb337aa66d38
Paweł <pawelnackowski@gmail.com>
2023-04-26 12:21:55 +0200

    changed name lightBlue to veryLightBlue and added new color lightBlue

--- src/theme.js
+++ src/theme.js
@@ -1,13 +1,14 @@
 export const theme = {
-  
+
   color: {
     white: "#ffffff",
-    lightBlue: "#6E7E91",
+    veryLightBlue: "#6E7E91",
+    lightBlue: "#2188FF",
     blue: "#0366D6",
-    darkBlue:"#0366d633",
+    darkBlue: "#0366d633",
     lightGray: "#fbfbfe",
     gray: "#E5E5E5",
-    darkerGray:"#d1d5da4d",
+    darkerGray: "#d1d5da4d",
     black: "#252525",
   },
   breakpoints: {
