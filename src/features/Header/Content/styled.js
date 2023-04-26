97cdd6edb276d55dcc90f44c4eae41a7cb2bacce
Paweł <pawelnackowski@gmail.com>
2023-04-26 12:20:06 +0200

    formatted

--- src/features/Header/Content/styled.js
+++ src/features/Header/Content/styled.js
@@ -1,12 +1,12 @@
 import styled from "styled-components";
-import { ReactComponent as message } from "../../../common/images/Message.svg";
+import {ReactComponent as message} from "../../../common/Container/Message.svg";
 
 export const Wrapper = styled.div`
   align-self: center;
 `
 
 export const Header = styled.h1`
-  color: ${({ theme }) => theme.color.black};
+  color: ${({theme}) => theme.color.black};
 `
 
 export const Text = styled.div`
@@ -21,23 +21,23 @@ export const ButtonLink = styled.a`
   display: flex;
   justify-content: space-around;
   align-items: center;
-  color: ${({ theme }) => theme.color.white};
+  color: ${({theme}) => theme.color.white};
   font-size: 20px;
   text-decoration: none;
   width: 154px;
   height: 49px;
   margin-top: 32px;
   padding: 12px;
-  background-color: ${({ theme }) => theme.color.blue};
-  border: 1px ${({ theme }) => theme.color.darkGray};
+  background-color: ${({theme}) => theme.color.blue};
+  border: 1px ${({theme}) => theme.color.darkGray};
   border-radius: 4px;
   transition: 0.5s;
-  
-    &:hover {
-      box-shadow: 2px -2px 0px #8CC2FF, -2px 2px 0px #8CC2FF, 2px 2px 0px #8CC2FF, -2px -2px 0px #8CC2FF;
-    }
 
-    &:active {
-      box-shadow: inset 0px 2px 0px rgba(20, 70, 32, 0.2);
-    }
+  &:hover {
+    box-shadow: 2px -2px 0px #8CC2FF, -2px 2px 0px #8CC2FF, 2px 2px 0px #8CC2FF, -2px -2px 0px #8CC2FF;
+  }
+
+  &:active {
+    box-shadow: inset 0px 2px 0px rgba(20, 70, 32, 0.2);
+  }
 `
