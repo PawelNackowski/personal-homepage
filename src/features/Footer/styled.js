31cc687d9b1eb6e7886d59c6b6613086fdaaea5b
Paweł <pawelnackowski@gmail.com>
2023-04-26 16:24:39 +0200

    added hover and active styled headerLink

--- src/features/Footer/styled.js
+++ src/features/Footer/styled.js
@@ -3,17 +3,25 @@ import styled from "styled-components";
 export const Container = styled.div`
   margin: 120px 0 109px 0;
   max-width: 670px;
-`
+`;
 export const Header = styled.h2`
   cursor: pointer;
-`
+`;
 export const HeaderLink = styled.a`
   font-size: 32px;
-  color: ${({theme}) => theme.color.black};
+  color: ${({ theme }) => theme.color.black};
   text-decoration: none;
-`
+
+  :hover {
+    color: ${({ theme }) => theme.color.blue};
+  }
+
+  :active {
+    color: ${({ theme }) => theme.color.lightBlue};
+  }
+`;
 export const Content = styled.div`
   font-size: 18px;
-  color: ${({theme}) => theme.color.black};
+  color: ${({ theme }) => theme.color.black};
   line-height: 25px;
-`
+`;
