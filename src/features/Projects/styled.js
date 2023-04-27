14b8d8d42a42d0208a966962ccaacc608bf9c129
Paweł <pawelnackowski@gmail.com>
2023-04-27 10:48:04 +0200

    added styled

--- src/features/Projects/styled.js
+++ src/features/Projects/styled.js
@@ -1,7 +1,33 @@
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export const StyledTile = styled.div`
+  background-color: ${({ theme }) => theme.color.white};
+  border: 6px solid rgba(209, 213, 218, 0.3);
+  box-shadow: 0px -2px 50px rgba(9, 10, 51, 0.02), 0px 16px 58px rgba(9, 10, 51, 0.03);
+  border-radius: 4px;
+  padding: 56px;
+  transition: 1s;
+
+  &:hover {
+    border: 6px solid rgba(3, 102, 214, 0.2);
+    box-shadow: 0px -2px 50px rgba(9, 10, 51, 0.02), 0px 16px 58px rgba(9, 10, 51, 0.03);
+  }
+`;
+export const SkillsContainer = styled.div`
   display: grid;
-  grid-template-columns: 1fr 1fr;
-  grid-gap: 32px;
-`
+  grid-template-columns: repeat(2, 1fr);
+  grid-template-rows: repeat(2, 1fr);
+  grid-column-gap: 32px;
+  grid-row-gap: 32px;
+  margin: 0 auto;
+`;
+
+export const StyledLink = styled.a`
+`;
