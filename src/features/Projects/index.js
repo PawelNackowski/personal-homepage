90f8343dcfd6a426cf893d4f8f4e098058c7abf9
Paweł <pawelnackowski@gmail.com>
2023-04-27 10:47:35 +0200

    changed render content in ProjectsSection

--- src/features/Projects/index.js
+++ src/features/Projects/index.js
@@ -1,35 +1,41 @@
-import { Tile } from "./Tile";
-import { Wrapper } from "./styled";
+import { SkillsContainer, StyledLink, StyledTile, Wrapper } from "./styled";
+import { Loading } from "./Loading";
+import { Error } from "./Error";
+import useRepos from "./useReps";
 
 export const ProjectSection = () => {
+  const { repos, isLoading, error } = useRepos();
   return (
-    <>
-      <Wrapper>
-        <Tile
-          title={"Nagłówek"}
-          content={"content"}
-          linkDemo={"link"}
-          linkRepo={"link"}
-        />
-        <Tile
-          title={"Nagłówek"}
-          content={"content"}
-          linkDemo={"link"}
-          linkRepo={"link"}
-        />
-        <Tile
-          title={"Nagłówek"}
-          content={"content"}
-          linkDemo={"link"}
-          linkRepo={"link"}
-        />
-        <Tile
-          title={"Nagłówek"}
-          content={"content"}
-          linkDemo={"link"}
-          linkRepo={"link"}
-        />
-      </Wrapper>
-    </>
+
+    <Wrapper>
+      Portfolio <br />
+      My recent projects
+      {isLoading ? (
+        <Loading />
+      ) : error ? (
+        <Error />
+      ) : (
+        <SkillsContainer>
+          {repos.map((repo) => (
+            <StyledTile key={repo.id}>
+              <div>{repo.name}</div>
+              <div>{repo.description}</div>
+              <div>
+                Demo:
+                <StyledLink href={repo.homepage} target="_blank">
+                  {repo.homepage}
+                </StyledLink>
+              </div>
+              <div>
+                Code:
+                <div href={repo.html_url} target="_blank">
+                  {repo.html_url}
+                </div>
+              </div>
+            </StyledTile>
+          ))}
+        </SkillsContainer>
+      )}
+    </Wrapper>
   );
 };
