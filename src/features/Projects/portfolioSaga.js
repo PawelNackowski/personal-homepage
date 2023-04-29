2e15def8409d6d7da1b55ed64418eb9fe2a93368
Paweł <pawelnackowski@gmail.com>
2023-04-29 10:59:13 +0200

    created portfolioSaga

---
+++ src/features/Projects/portfolioSaga.js
@@ -0,0 +1,20 @@
+import { delay, put, call, takeLatest } from "redux-saga/effects";
+import { fetchPortfolio, fetchPortfolioError, fetchPortfolioSuccess } from "./portfolioSlice";
+import { getPortfolio } from "./getPortfolio";
+
+const DELAY_ACTION = 2000;
+
+function* watchFetchPortfolioHandler() {
+  try {
+    yield delay(DELAY_ACTION);
+    const portfolio = yield call(getPortfolio);
+    yield put(fetchPortfolioSuccess(portfolio));
+  } catch (error) {
+    yield put(fetchPortfolioError());
+  }
+  ;
+};
+
+export function* watchFetchPortfolio() {
+  yield takeLatest(fetchPortfolio.type, watchFetchPortfolioHandler);
+};
