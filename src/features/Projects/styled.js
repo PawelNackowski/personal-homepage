3862010d664c14adf6110f4404c06f4c54401bc5
Paweł <pawelnackowski@gmail.com>
2023-04-29 11:03:44 +0200

    added styled SkillsContainer

--- src/features/Projects/styled.js
+++ src/features/Projects/styled.js
@@ -7,7 +7,7 @@ export const Wrapper = styled.div`
   align-items: center;
 `;
 
-export const StyledTile = styled.div`
+export const StyledTile = styled.li`
   background-color: ${({ theme }) => theme.color.white};
   border: 6px solid rgba(209, 213, 218, 0.3);
   box-shadow: 0px -2px 50px rgba(9, 10, 51, 0.02), 0px 16px 58px rgba(9, 10, 51, 0.03);
@@ -19,14 +19,6 @@ export const StyledTile = styled.div`
     border: 6px solid rgba(3, 102, 214, 0.2);
     box-shadow: 0px -2px 50px rgba(9, 10, 51, 0.02), 0px 16px 58px rgba(9, 10, 51, 0.03);
   }
-`;
-export const SkillsContainer = styled.div`
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  grid-template-rows: repeat(2, 1fr);
-  grid-column-gap: 32px;
-  grid-row-gap: 32px;
-  margin: 0 auto;
 `;
 
 export const StyledLink = styled.a`
