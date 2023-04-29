e6e548f3c5902f91b6642c33ce845853952f1dc2
Paweł <pawelnackowski@gmail.com>
2023-04-29 10:59:24 +0200

    created portfolioSlice

---
+++ src/features/Projects/portfolioSlice.js
@@ -0,0 +1,35 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const portfolioSlice = createSlice({
+  name: "portfolio",
+  initialState: {
+    portfolio: [],
+    portfolioState: "loading"
+  },
+  reducers: {
+    fetchPortfolio: () => ({
+      portfolioState: "loading",
+      portfolio: null
+    }),
+    fetchPortfolioSuccess: (_, { payload: portfolio }) => ({
+      portfolioState: "success",
+      portfolio: portfolio
+    }),
+    fetchPortfolioError: () => ({
+      portfolioState: "error",
+      portfolio: null
+    })
+  }
+});
+
+export const {
+  fetchPortfolio,
+  fetchPortfolioSuccess,
+  fetchPortfolioError
+} = portfolioSlice.actions;
+
+export const selectPortfolioSliceState = state => state.portfolio;
+export const selectPortfolio = state => selectPortfolioSliceState(state).portfolio;
+export const selectPortfolioState = state => selectPortfolioSliceState(state).portfolioState;
+
+export default portfolioSlice.reducer;
