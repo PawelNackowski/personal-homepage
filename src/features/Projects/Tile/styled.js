0725273a68ed9f874d2f104058cef6a79a359237
Paweł <pawelnackowski@gmail.com>
2023-04-29 11:02:11 +0200

    changed styled wrapper, changed wrapper -> List and link div -> a

--- src/features/Projects/Tile/styled.js
+++ src/features/Projects/Tile/styled.js
@@ -1,29 +1,26 @@
 import styled from "styled-components";
 
-export const Wrapper = styled.div`
-  background-color: ${({ theme }) => theme.color.white};
-  border: 6px solid rgba(209, 213, 218, 0.3);
-  box-shadow: 0px -2px 50px rgba(9, 10, 51, 0.02), 0px 16px 58px rgba(9, 10, 51, 0.03);
-  border-radius: 4px;
-  padding: 56px;
-  transition: 1s;
-
-    &:hover{
-     border: 6px solid rgba(3, 102, 214, 0.2);
-      box-shadow: 0px -2px 50px rgba(9, 10, 51, 0.02), 0px 16px 58px rgba(9, 10, 51, 0.03);
-    }
-`
+export const List = styled.ul`
+  max-width: 1215px;
+  margin: 24px 0 120px;
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  list-style: none;
+  padding: 0;
+  grid-gap: 32px;
+`;
 
 export const Header = styled.h2`
   margin: 0;
   font-size: 24px;
   color: ${({ theme }) => theme.color.blue};
-`
+`;
 
 export const Descroption = styled.div`
   margin: 24px 0 16px 0;
-`
+`;
 
-export const Link = styled.div`
-  margin-top: 8xp;
-`
+export const Link = styled.a`
+  margin-top: 8px;
+`;
