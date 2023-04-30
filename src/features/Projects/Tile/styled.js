9af42549b34a378ac4580c7d47f25faa4ad1f9b8
Paweł <pawelnackowski@gmail.com>
2023-04-30 10:02:29 +0200

    changed Descroption -> Description and Link styled

--- src/features/Projects/Tile/styled.js
+++ src/features/Projects/Tile/styled.js
@@ -1,12 +1,11 @@
 import styled from "styled-components";
 
-export const List = styled.ul`
+export const List = styled.div`
   max-width: 1215px;
   margin: 24px 0 120px;
   width: 100%;
   display: grid;
   grid-template-columns: repeat(2, 1fr);
-  list-style: none;
   padding: 0;
   grid-gap: 32px;
 `;
@@ -17,10 +16,42 @@ export const Header = styled.h2`
   color: ${({ theme }) => theme.color.blue};
 `;
 
-export const Descroption = styled.div`
+export const Description = styled.div`
   margin: 24px 0 16px 0;
+  font-size: 18px;
 `;
 
 export const Link = styled.a`
+  color: ${({ theme }) => theme.color.blue};
+  margin-left: 14px;
+  text-decoration: none;
+  border-bottom: 1px solid ${({ theme }) => theme.color.gray};
+
+  &:hover {
+    color: ${({ theme }) => theme.color.lightBlue};
+    border-color: ${({ theme }) => theme.color.lightBlue};
+    
+  }
+`;
+
+export const WrapperLink = styled.div`
+  font-size: 18px;
+  display: flex;
   margin-top: 8px;
+`;
+
+export const StyledTile = styled.div`
+  display: grid;
+  grid-template-rows: auto 1fr auto;
+  background-color: ${({ theme }) => theme.color.white};
+  border: 6px solid rgba(209, 213, 218, 0.3);
+  box-shadow: 0px -2px 50px rgba(9, 10, 51, 0.02), 0px 16px 58px rgba(9, 10, 51, 0.03);
+  border-radius: 4px;
+  padding: 56px;
+  transition: 1s;
+
+  &:hover {
+    border: 6px solid rgba(3, 102, 214, 0.2);
+    box-shadow: 0px -2px 50px rgba(9, 10, 51, 0.02), 0px 16px 58px rgba(9, 10, 51, 0.03);
+  }
 `;
