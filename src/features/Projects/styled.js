f28f87b8277fd78d004193e10dcd82b9814ac020
Paweł <pawelnackowski@gmail.com>
2023-04-30 10:03:13 +0200

    added StyledTile

--- src/features/Projects/styled.js
+++ src/features/Projects/styled.js
@@ -1,25 +1,8 @@
 import styled from "styled-components";
-import { Link } from "react-router-dom";
 
 export const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-`;
 
-export const StyledTile = styled.li`
-  background-color: ${({ theme }) => theme.color.white};
-  border: 6px solid rgba(209, 213, 218, 0.3);
-  box-shadow: 0px -2px 50px rgba(9, 10, 51, 0.02), 0px 16px 58px rgba(9, 10, 51, 0.03);
-  border-radius: 4px;
-  padding: 56px;
-  transition: 1s;
-
-  &:hover {
-    border: 6px solid rgba(3, 102, 214, 0.2);
-    box-shadow: 0px -2px 50px rgba(9, 10, 51, 0.02), 0px 16px 58px rgba(9, 10, 51, 0.03);
-  }
-`;
-
-export const StyledLink = styled.a`
 `;
