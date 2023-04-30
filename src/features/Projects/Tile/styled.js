f833d8923504b9f3b5ddd0d520315cb40d8ba014
Paweł <pawelnackowski@gmail.com>
2023-04-30 11:44:01 +0200

    added media query

--- src/features/Projects/Tile/styled.js
+++ src/features/Projects/Tile/styled.js
@@ -7,17 +7,30 @@ export const List = styled.div`
   grid-template-columns: repeat(2, 1fr);
   padding: 0;
   grid-gap: 32px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+    grid-template-columns: 1fr;
+    grid-gap: 16px;
+  }
 `;
 
 export const Header = styled.h2`
   margin: 0;
   font-size: 24px;
   color: ${({ theme }) => theme.color.blue};
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+    font-size: 16px;
+  }
 `;
 
 export const Description = styled.div`
   margin: 24px 0 16px 0;
   font-size: 18px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+    font-size: 14px;
+  }
 `;
 
 export const Link = styled.a`
@@ -26,18 +39,22 @@ export const Link = styled.a`
   text-decoration: none;
   border-bottom: 1px solid ${({ theme }) => theme.color.gray};
   transition: 0.5s;
+}
 
-  &:hover {
-    color: ${({ theme }) => theme.color.lightBlue};
-    border-color: ${({ theme }) => theme.color.lightBlue};
+&:hover {
+  color: ${({ theme }) => theme.color.lightBlue};
+  border-color: ${({ theme }) => theme.color.lightBlue};
 
-  }
+}
 `;
 
 export const WrapperLink = styled.div`
   font-size: 18px;
   display: flex;
   margin-top: 8px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+    font-size: 14px;
 `;
 
 export const StyledTile = styled.div`
@@ -48,10 +65,14 @@ export const StyledTile = styled.div`
   box-shadow: 0px -2px 50px rgba(9, 10, 51, 0.02), 0px 16px 58px rgba(9, 10, 51, 0.03);
   border-radius: 4px;
   padding: 56px;
-  transition: 0.5s;
 
   &:hover {
+    transition: 0.5s;
     border: 6px solid rgba(3, 102, 214, 0.2);
     box-shadow: 0px -2px 50px rgba(9, 10, 51, 0.02), 0px 16px 58px rgba(9, 10, 51, 0.03);
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+    padding: 24px;
   }
 `;
