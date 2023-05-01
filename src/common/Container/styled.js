22bbcadfc96ffec9c13f0f0a1f7614315d8a832b
Paweł <pawelnackowski@gmail.com>
2023-05-01 10:08:04 +0200

    added media query

--- src/common/Container/styled.js
+++ src/common/Container/styled.js
@@ -3,9 +3,14 @@ import styled from "styled-components";
 export const Wrapper = styled.div`
   background-color: ${({ theme }) => theme.color.white};
   padding: 32px;
-  margin: 72px auto;
+  margin: 64px 14px 0 14px;
   box-shadow: 0px -2px 50px rgba(9, 10, 51, 0.02), 0px 16px 58px rgba(9, 10, 51, 0.03);
   border-radius: 4px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+    padding: 16px;
+    margin-top: 48px;
+  }
 `;
 
 export const Header = styled.h2`
@@ -14,9 +19,23 @@ export const Header = styled.h2`
   font-size: 30px;
   border-bottom: 1px solid ${({ theme }) => theme.color.darkerGray};
   padding-bottom: 15px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+    font-size: 22px;
+  }
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMedium}px) {
+    font-size: 18px;
+  }
 `;
 
 export const Body = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+    grid-template-columns: 1fr 1fr;
+  }
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMedium}px) {
+    grid-template-columns: 1fr;
+  }
 `;
--- src/features/Lists/styled.js
+++ src/features/Lists/styled.js
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const ListItem = styled.li`
   display: flex;
-  padding: 8px 8px 8px 0;
+  padding: 8px;
   font-size: 18px;
 
   ::before {
@@ -12,5 +12,12 @@ export const ListItem = styled.li`
     display: inline-block;
     width: 1em;
     margin-left: -1em;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+    font-size: 16px;
+  }
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMedium}px) {
+    font-size: 14px;
   }
 `;
