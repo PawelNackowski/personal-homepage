1d40f0800fb85f7c2a78cc88d9c7124d03d6c7ae
Paweł <pawelnackowski@gmail.com>
2023-05-04 11:36:36 +0200

    deleted unnecessary fetch

--- src/rootSaga.js
+++ src/rootSaga.js
@@ -1,8 +1,5 @@
 import { all } from "redux-saga/effects";
-import { watchFetchPortfolio } from "./PersonalPage/Projects/portfolioSaga";
 
 export default function* rootSaga() {
-  yield all([
-    watchFetchPortfolio()
-  ]);
+  yield all([]);
 };
