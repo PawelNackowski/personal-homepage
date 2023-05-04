1a6fbc0c99d39af9768dc5ef91f05aa001edea35
Paweł <pawelnackowski@gmail.com>
2023-05-04 09:59:38 +0200

    added themeProvider and globalStyle

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from "react";
 import { PersonalPage } from "./PersonalPage";
+import { ThemeProvider } from "styled-components";
+import { dark, light } from "./theme";
+import { GlobalStyle } from "./GlobalStyle";
+import { useSelector } from "react-redux";
+import { selectDarkTheme } from "./PersonalPage/Header/Content/ThemeButton/themeSlice";
 
 function App() {
+  const darkTheme = useSelector(selectDarkTheme);
   return (
-    <PersonalPage />
+    <ThemeProvider theme={darkTheme ? dark : light}>
+      <GlobalStyle />
+      <PersonalPage />
+    </ThemeProvider>
   );
 }
 
