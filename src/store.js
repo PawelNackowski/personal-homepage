7bd5f9d05829a0557e7ede5b0c66f8fa90acd951
Paweł <pawelnackowski@gmail.com>
2023-05-04 10:06:03 +0200

    deleted theme reducer

--- src/store.js
+++ src/store.js
@@ -1,12 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import createSagaMiddleware from "redux-saga";
 import rootSaga from "./rootSaga";
+import themeReducers from "./PersonalPage/Header/Content/ThemeButton/themeSlice";
 import portfolioReducers from "./PersonalPage/Projects/portfolioSlice";
 
 const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
   reducer: {
+    theme: themeReducers,
     portfolio: portfolioReducers
   },
   middleware: [sagaMiddleware]
