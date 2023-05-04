18fface787824f8eaba1b324e12088d61bfd771f
Paweł <pawelnackowski@gmail.com>
2023-05-04 11:36:16 +0200

    deleted unnecessary reducer

--- src/store.js
+++ src/store.js
@@ -2,14 +2,12 @@ import { configureStore } from "@reduxjs/toolkit";
 import createSagaMiddleware from "redux-saga";
 import rootSaga from "./rootSaga";
 import themeReducers from "./PersonalPage/Header/Content/ThemeButton/themeSlice";
-import portfolioReducers from "./PersonalPage/Projects/portfolioSlice";
 
 const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
   reducer: {
-    theme: themeReducers,
-    portfolio: portfolioReducers
+    theme: themeReducers
   },
   middleware: [sagaMiddleware]
 });
