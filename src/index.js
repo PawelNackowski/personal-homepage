3a26abc7236141160afafe06b4e75387162cac25
Paweł <pawelnackowski@gmail.com>
2023-05-04 10:33:46 +0200

    added queryClient

--- src/index.js
+++ src/index.js
@@ -4,14 +4,16 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import store from "./store";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+const queryClient = new QueryClient();
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <QueryClientProvider client={queryClient}>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>
+  </QueryClientProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
