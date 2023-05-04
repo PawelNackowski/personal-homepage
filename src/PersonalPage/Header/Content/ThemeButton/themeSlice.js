5f757f468b2a59fce141a08a4e9450ca2443390b
Paweł <pawelnackowski@gmail.com>
2023-05-04 10:08:28 +0200

    create theme slice

---
+++ src/PersonalPage/Header/Content/ThemeButton/themeSlice.js
@@ -0,0 +1,18 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const themeSlice = createSlice({
+  name: "theme",
+  initialState: {
+    darkTheme: true
+  },
+  reducers: {
+    toggleTheme: state => {
+      state.darkTheme = !state.darkTheme;
+    }
+  }
+});
+
+export const { toggleTheme } = themeSlice.actions;
+export const selectThemeState = state => state.theme;
+export const selectDarkTheme = state => selectThemeState(state).darkTheme;
+export default themeSlice.reducer;
