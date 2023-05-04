7e6c10261aafe4724603f52d02a4424e97ce9505
Paweł <pawelnackowski@gmail.com>
2023-05-04 10:01:44 +0200

    deleted themeProvider and globalStyle

--- src/index.js
+++ src/index.js
@@ -4,19 +4,13 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import store from "./store";
-import { ThemeProvider } from "styled-components";
-import { light } from "./theme";
-import { GlobalStyle } from "./GlobalStyle";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={light}>
-      <GlobalStyle />
-      <Provider store={store}>
-        <App />
-      </Provider>
-    </ThemeProvider>
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
 
