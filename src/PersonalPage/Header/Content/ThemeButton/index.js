95d33a28043db1e57fa7e37de481ff2d654c94f9
Paweł <pawelnackowski@gmail.com>
2023-05-04 10:01:12 +0200

    added toggleTheme on button

--- src/PersonalPage/Header/Content/ThemeButton/index.js
+++ src/PersonalPage/Header/Content/ThemeButton/index.js
@@ -1,12 +1,18 @@
-import { ThemeModeButton, ModeButton, Wrapper } from './styled';
+import { ThemeModeButton, ModeButton, Wrapper, ButtonWrapper } from "./styled";
+import { selectDarkTheme, toggleTheme } from "./themeSlice";
+import { useDispatch, useSelector } from "react-redux";
 
 export const ThemeButton = () => {
+  const dispatch = useDispatch();
+  const isDarkTheme = useSelector(selectDarkTheme);
   return (
-      <ThemeModeButton>
-        DARK MODE OFF
-        <Wrapper>
-        <ModeButton />
-        </Wrapper>
-      </ThemeModeButton>
-  )
+    <ThemeModeButton onClick={() => dispatch(toggleTheme())}>
+      DARK MODE {isDarkTheme ? "ON" : "OFF"}
+      <Wrapper>
+        <ButtonWrapper moveToRight={isDarkTheme}>
+          <ModeButton />
+        </ButtonWrapper>
+      </Wrapper>
+    </ThemeModeButton>
+  );
 };
