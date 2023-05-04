7e47095f84eaf917b2e98cba658e03f5e1dee379
Paweł <pawelnackowski@gmail.com>
2023-05-04 11:34:59 +0200

    changed method render tile

--- src/PersonalPage/Projects/index.js
+++ src/PersonalPage/Projects/index.js
@@ -1,27 +1,13 @@
 import { Wrapper, Header, SubHeader, GitHubRepoIcon } from "./styled";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchPortfolio, selectPortfolio, selectPortfolioState } from "./portfolioSlice";
-import { useEffect } from "react";
-import { Project } from "./porfolioState";
+import { Tile } from "./Tile";
 
 export const ProjectSection = () => {
-  const dispatch = useDispatch();
-  const portfolioState = useSelector(selectPortfolioState);
-  const portfolio = useSelector(selectPortfolio);
-
-  useEffect(() => {
-    dispatch(fetchPortfolio());
-  }, [dispatch]);
   return (
-
     <Wrapper>
       <GitHubRepoIcon />
       <Header>Portfolio</Header>
       <SubHeader>My recent projects</SubHeader>
-      <Project
-        portfolio={portfolio}
-        portfolioState={portfolioState}
-      />
+      <Tile />
     </Wrapper>
   );
 };
