86719cc2c7521d69e23e456d75d63a585ec690fc
Paweł <pawelnackowski@gmail.com>
2023-05-06 11:36:07 +0200

    changed format

--- src/PersonalPage/Header/Content/index.js
+++ src/PersonalPage/Header/Content/index.js
@@ -2,23 +2,19 @@ import { ButtonLink, Header, MessageIcon, Text, Wrapper } from "./styled";
 import { description, name, email } from "../../../namePlate";
 import { ThemeButton } from "./ThemeButton";
 
-export const Content = () => {
-  return (
-    <>
-      <Wrapper>
-        <ThemeButton />
-        THIS IS
-        <Header>
-          {name}
-        </Header>
-        <Text>
-          {description}
-        </Text>
-        <ButtonLink title={email} href={`mailto:${email}`}>
-          <MessageIcon />
-          Hire Me
-        </ButtonLink>
-      </Wrapper>
-    </>
-  );
-};
+export const Content = () => (
+  <Wrapper>
+    <ThemeButton />
+    THIS IS
+    <Header>
+      {name}
+    </Header>
+    <Text>
+      {description}
+    </Text>
+    <ButtonLink title={email} href={`mailto:${email}`}>
+      <MessageIcon />
+      Hire Me
+    </ButtonLink>
+  </Wrapper>
+);
