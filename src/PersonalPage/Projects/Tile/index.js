e1c06ce768f251bcc0645087caf1625ffab90253
Paweł <pawelnackowski@gmail.com>
2023-05-06 11:27:41 +0200

    changed error status

--- src/PersonalPage/Projects/Tile/index.js
+++ src/PersonalPage/Projects/Tile/index.js
@@ -5,41 +5,40 @@ import { fetchPortfolio } from "../fetchPortfolio";
 import { Loading } from "../Loading";
 import { Error } from "../Error";
 
-export const Tile = ({ portfolio }) => {
-  const { isLoading, data, error } = useQuery(
+const HIDDEN_REPOS = [
+  "PawelNackowski"
+];
+
+export const Tile = () => {
+  const { isLoading, data, isError } = useQuery(
     ["repos"],
     fetchPortfolio
   );
-  const HIDDEN_REPOS = [
-    "PawelNackowski"
-  ];
   if (isLoading) {
     return <Loading />;
   }
-  if (isError(error)) {
+  if (isError) {
     return <Error />;
   }
   return (
-    <>
-      <List>
-        {data.map(({ id, name, description, homepage, html_url }) => (
-          !!HIDDEN_REPOS.includes(name) ? null : (
-            <StyledTile key={id}>
-              <Header>{name}</Header>
-              <Description>{description}</Description>
-              {!!homepage && (
-                <WrapperLink>
-                  Demo:
-                  <Link href={homepage} target="_blank" rel="noreferrer">Project Demo</Link>
-                </WrapperLink>
-              )}
+    <List>
+      {data.map(({ id, name, description, homepage, html_url }) => (
+        !!HIDDEN_REPOS.includes(name) ? null : (
+          <StyledTile key={id}>
+            <Header>{name}</Header>
+            <Description>{description}</Description>
+            {!!homepage && (
               <WrapperLink>
-                Code:
-                <Link href={html_url} target="_blank" rel="noreferrer"> Link to code</Link>
+                Demo:
+                <Link href={homepage} target="_blank" rel="noreferrer">Project Demo</Link>
               </WrapperLink>
-            </StyledTile>
-          )))}
-      </List>
-    </>
+            )}
+            <WrapperLink>
+              Code:
+              <Link href={html_url} target="_blank" rel="noreferrer"> Link to code</Link>
+            </WrapperLink>
+          </StyledTile>
+        )))}
+    </List>
   );
 };
