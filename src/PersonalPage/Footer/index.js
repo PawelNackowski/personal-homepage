fdf80d99d5d75b88abe14a86c6ace323e9a25094
Paweł <pawelnackowski@gmail.com>
2023-05-06 11:35:00 +0200

    changed format

--- src/PersonalPage/Footer/index.js
+++ src/PersonalPage/Footer/index.js
@@ -2,21 +2,17 @@ import { Socials } from "../Socjals";
 import { email, footerContent } from "../../namePlate";
 import { Container, Content, Header, HeaderLink } from "./styled";
 
-export const Footer = () => {
-  return (
-    <>
-      <Container>
-        LETS TALK!
-        <Header>
-          <HeaderLink href={`mailto:${email}`}>
-            {email}
-          </HeaderLink>
-        </Header>
-        <Content>
-          {footerContent}
-        </Content>
-        <Socials />
-      </Container>
-    </>
-  );
-};
+export const Footer = () => (
+  <Container>
+    LETS TALK!
+    <Header>
+      <HeaderLink href={`mailto:${email}`}>
+        {email}
+      </HeaderLink>
+    </Header>
+    <Content>
+      {footerContent}
+    </Content>
+    <Socials />
+  </Container>
+);
