1bc17eae4e8f1b8247db59ba887b36b77e7d7ef7
Paweł <pawelnackowski@gmail.com>
2023-05-07 11:08:37 +0200

    changed render body in section

--- src/feature/PersonalPage/index.js
+++ src/feature/PersonalPage/index.js
@@ -1,21 +1,20 @@
 import { Header } from "./Header";
 import { Section } from "../../common/Section";
 import { goalsHeader, skillsHeader } from "../../namePlate";
-import { SkillsList } from "./Lists/SkillsList";
-import { FeatureList } from "./Lists/FeatureList";
 import { ProjectSection } from "./Projects";
 import { Footer } from "./Footer";
 import React from "react";
+import { List } from "./Lists/List";
 
 export const PersonalPage = () => (
   <>
     <Header />
     <Section
       title={skillsHeader}
-      body={<SkillsList />} />
+      body={<List type={"skill"} />} />
     <Section
       title={goalsHeader}
-      body={<FeatureList />}
+      body={<List type={"goal"} />}
     />
     <ProjectSection />
     <Footer />
